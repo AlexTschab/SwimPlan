6d6af75aaf90538e708c5024538bb6c6708c486b
Alexandre Tschabuschnig <alexandre.tschabuschnig@gmail.com>
2023-04-24 13:53:30 +0200

    start form to modifiy and create trainings

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,13 +1,24 @@
 class TrainingsController < ApplicationController
-
   def index
+    @trainings = Training.all
   end
 
   def show
+    @training = Training.find(params[:id])
+    @bloc = Bloc.new
+    @bloc.training = @training
   end
 
   def new
     @training = Training.new
   end
 
+  def create
+    @training = Training.new
+    @training.save
+  end
+
+  def edit
+    @training = Training.find(params[:id])
+  end
 end
