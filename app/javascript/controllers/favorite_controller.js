540a205c23d8a56e620b60b870e5f60aa337dcc4
Alexandre Tschabuschnig <alexandre.tschabuschnig@gmail.com>
2023-05-15 13:13:30 +0200

    js for star

--- app/javascript/controllers/favorite_controller.js
+++ app/javascript/controllers/favorite_controller.js
@@ -2,7 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="favorite"
 export default class extends Controller {
+  static targets = [ "star"]
   connect() {
     console.log("test")
+  }
+
+  toggle() {
+    console.log("clicked on star")
+
+    this.starTarget.classList.toggle("fa-2xl")
+    console.log(this.starTarget.innerhtml)
+
   }
 }
