086d8b977e5b22ab4e7b09636e87e20b9e818c6c
Alexandre Tschabuschnig <alexandre.tschabuschnig@gmail.com>
2023-05-22 16:32:36 +0200

    js for star click

--- app/javascript/controllers/favorite_controller.js
+++ app/javascript/controllers/favorite_controller.js
@@ -3,16 +3,16 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="favorite"
 export default class extends Controller {
   static targets = [ "star"]
-  connect() {
-    console.log("test")
-  }
 
   toggle() {
     console.log("clicked on star")
     const star = this.starTarget;
 
     if (star.classList.contains("fa-regular")) {
-      console.log("ok");
+      this.addFavorite();
+    }
+    else {
+      this.removeFavorite();
     }
 
     this.starTarget.classList.toggle("yellow-star");
@@ -20,15 +20,18 @@ export default class extends Controller {
     this.starTarget.classList.toggle("fa-solid");
 
     console.log(this.starTarget)
-
   }
 
   addFavorite() {
     console.log("not fav");
+    const trainingId = this.data.get("trainingId"); // Retrieve the training ID from data attribute
+    console.log(trainingId);
 
+    // Add your logic to add the favorite here
   }
 
   removeFavorite() {
     console.log("fav");
+    // Add your logic to remove the favorite here
   }
 }
