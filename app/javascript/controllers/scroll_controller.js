f6518170c18c5ee77b027f0f3c3a36a0ad79ab74
Alexandre Tschabuschnig <alexandre.tschabuschnig@gmail.com>
2023-06-04 09:28:23 +0200

    fixed the arrows to disapear when not possible to scroll

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -4,74 +4,81 @@ export default class extends Controller {
   static targets = ["cards"];
 
   connect() {
-    console.log("Hello World!3");
-    this.updateScrollButtons();
+    console.log("Hello World!1");
+    console.log(this.updateScrollButtons());
   }
 
   scrollLeft() {
-    console.log("Hello from left scroll click");
     const cardWidth = this.getCardWidth();
-    this.cardsTarget.scrollLeft -= cardWidth;
-    // this.updateScrollButtons();
+
+    if (this.updateScrollButtons()[0]) {
+      this.cardsTarget.scrollLeft -= cardWidth;
+    }
+    this.updateScrollButtons();
+
   }
 
   scrollRight() {
-    console.log("Hello from right scroll click");
     const cardWidth = this.getCardWidth();
-    this.cardsTarget.scrollLeft += cardWidth;
-    // this.updateScrollButtons();
+    if (this.updateScrollButtons()[1]) {
+      this.cardsTarget.scrollLeft += cardWidth;
+    }
+    this.updateScrollButtons();
   }
 
   getCardWidth() {
     const cardElement = this.cardsTarget.querySelector('.card');
     const cardStyle = window.getComputedStyle(cardElement);
-
-    //console.log(cardStyle.width);
-    console.log(cardStyle.marginRight);
-    console.log(cardStyle.marginLeft);
-    console.log(parseFloat(cardStyle.width) + parseFloat(cardStyle.marginLeft));
     const cardWidth =
         parseFloat(cardStyle.width)
       + parseFloat(cardStyle.marginRight)
       + parseFloat(cardStyle.marginLeft)
       + parseFloat(cardStyle.paddingRight)
       + parseFloat(cardStyle.paddingLeft);
-      console.log(cardWidth)
+
     return cardWidth;
   }
 
   updateScrollButtons() {
+
     const cardWidth = this.getCardWidth();
     const availableWidth = this.cardsTarget.offsetWidth;
+
     const scrollWidth = this.cardsTarget.scrollWidth;
+    // scroll width increases as we add trainings, it tells how width the total width with all cards is
     const scrollLeft = this.cardsTarget.scrollLeft;
-
-    let canScrollLeft = scrollLeft > 0;
-    let canScrollRight = scrollLeft + availableWidth < cardWidth;
-
-    //tests
-    console.log(this.cardsTarget);
-    console.log(cardWidth);
-
-
-    console.log("scrollLeft");
-    console.log(scrollLeft);
-    console.log("can scroll left");
-    console.log(canScrollLeft);
-    console.log("can scroll right");
-    console.log(canScrollRight);
 
     // Calculate how much is left to scroll on both sides
     const remainingScrollLeft = scrollLeft;
     const remainingScrollRight = scrollWidth - scrollLeft - availableWidth;
 
-    console.log(`Remaining Scroll Left: ${remainingScrollLeft}px`);
-    console.log(`Remaining Scroll Right: ${remainingScrollRight}px`);
+    let canScrollLeft = (remainingScrollLeft - cardWidth) > -5
+    let canScrollRight = (remainingScrollRight - cardWidth) > -5 ;
+
+    const rightButton = document.querySelector(".right-arrow");
+    console.log(rightButton);
+    if (!canScrollRight && !rightButton.classList.contains("hidden")) {
+      rightButton.classList.add("hidden")
+    } else if (canScrollRight && rightButton.classList.contains("hidden")) {
+      rightButton.classList.remove("hidden")
+    };
+
+    const leftButton = document.querySelector(".left-arrow");
+    console.log(leftButton);
+    if (!canScrollLeft && !leftButton.classList.contains("hidden")) {
+      leftButton.classList.add("hidden")
+    }else if (canScrollLeft && leftButton.classList.contains("hidden")) {
+      leftButton.classList.remove("hidden")
+    };
+
+
+
+    return [canScrollLeft,canScrollRight]
+
 
     // Disable or enable scroll buttons based on scrollability
-    const leftButton = document.querySelector(".left-arrow");
-    const rightButton = document.querySelector(".right-arrow");
-    leftButton.disabled = !canScrollLeft;
-    rightButton.disabled = !canScrollRight;
+
+    //leftButton.disabled = !canScrollLeft;
+    // rightButton.disabled = !canScrollRight;
   }
 }
